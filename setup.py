ef5f492c505cb0b6d9342bb66564c086fa891410
metavinci <pszdw-75nat-5227i-bha5s-y7lai-pebdq-o2agp-3xho4-hd6z6-emxrd-nqe@dmail.ai>
2025-02-08 21:58:32 -0800

    Separating QT UI used in hvym into it's own package, so it can be used in other projects

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import setuptools
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "qthvym",
+    version = "0.0.1",
+    author = "Fibo Metavinci",
+    author_email = "pszdw-75nat-5227i-bha5s-y7lai-pebdq-o2agp-3xho4-hd6z6-emxrd-nqe@dmail.ai",
+    description = "QT UI for HVYM.",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/inviti8",
+    project_urls = {
+        "Bug Tracker": "https://github.com/inviti8/gifanimus/issues",
+    },
+    package_dir = {"": "qthvym"},
+    packages = setuptools.find_packages(where="qthvym"),
+    include_package_data=True, 
+    python_requires = ">=3.9.18"
+)
---
+++ test.py
@@ -0,0 +1,76 @@
+from qthvym import *
+
+
+def splash():
+    interaction = HVYMInteraction()
+    interaction.splash("TEST!!")
+
+def msg_popup():
+      interaction = HVYMInteraction()
+      interaction.msg_popup("TEST!!!")
+
+def options_popup():
+      interaction = HVYMInteraction()
+      interaction.options_popup("TEST!!!!!", ['1', '2', '3'])
+      
+      return interaction
+
+def user_popup():
+      interaction = HVYMInteraction()
+      interaction.user_popup("TEST")
+
+      return interaction
+
+def password_popup():
+      interaction = HVYMInteraction()
+      interaction.password_popup("TEST")
+
+      return interaction
+
+def user_password_popup():
+      interaction = HVYMInteraction()
+      interaction.user_password_popup("TEST", "defaultText")
+
+      return interaction
+
+def copy_line_popup():
+      interaction = HVYMInteraction()
+      interaction.copy_line_popup("TEST", "defaultText")
+
+      return interaction
+
+def copy_text_popup():
+      interaction = HVYMInteraction()
+      interaction.copy_text_popup("TEST", "defaultText")
+
+      return interaction
+
+def choice_popup():
+      """ Show choice popup, message based on passed msg arg."""
+      interaction = HVYMInteraction()
+      interaction.choice_popup("TEST?")
+
+      return interaction
+
+def file_select_popup():
+      interaction = HVYMInteraction()
+      interaction.file_select_popup("TEST", ["Images (*.png *.svg)"])
+
+      return interaction
+
+
+#splash()
+#msg_popup()
+#print(options_popup().value)
+#print(user_popup().value)
+#print(password_popup().value)
+#print(user_password_popup().value)
+#print(copy_line_popup().value)
+#print(copy_text_popup().value)
+#print(choice_popup().value)
+#print(file_select_popup().value)
+    
+
+
+
+    
